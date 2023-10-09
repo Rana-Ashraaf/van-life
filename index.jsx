ca39fbbb61232d77aa648efd6910a5b85c48a30e
Rana <rannaashraf33@gmail.com>
2023-10-09 14:11:06 +0300

    layout-route

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { VanDetail } from "./pages/VanDetail";
 import { Vans } from "./pages/Vans";
 import "./server";
 
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
